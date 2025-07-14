728a99b91eb4350b03df5710cb7599b153d759d0
sajessop <jessop.sarah.a@gmail.com>
2025-07-14 15:44:52 +1000

    added options for parsing

--- R/parse_api.R
+++ R/parse_api.R
@@ -5,12 +5,15 @@
 #'
 #' @param response An \code{httr::response} object returned by the request or export call.
 #' @param filetype Optional string specifying the expected response format.
-#'   Defaults to \code{"json"}. Supported values are \code{"csv"} and \code{"json"}.
+#'   Defaults to \code{"json"}. Supported values are \code{"csv"}, \code{"json"}, \code{"html"} and \code{"txt"}.
+#' @param view_html Binary option to view html in default browser.
 #'
 #' @return A parsed R object:
 #' \itemize{
 #'   \item If \code{filetype = "csv"}: a \code{data.frame}.
 #'   \item If \code{filetype = "json"}: a list or data frame, depending on the JSON structure.
+#'   \item If \code{filetype = "html"}: a character string representing html text.
+#'   \item If \code{filetype = "txt"}: a character string representing txt text.
 #' }
 #'
 #' @details
@@ -19,6 +22,8 @@
 #' \itemize{
 #'   \item \code{"csv"}: Parses CSV content into a \code{data.frame} using \code{read.csv()}.
 #'   \item \code{"json"}: Parses JSON content into a list or \code{data.frame} using \code{jsonlite::fromJSON()}.
+#'   \item \code{"html"}: Reads html content into html character string.
+#'   \item \code{"txt"}: Reads txt content into character string.
 #' }
 #'
 #' @examples
@@ -57,9 +62,8 @@
 #' }
 #'
 #' @export
-parse_api <- function(response, filetype = NULL) {
-  filetype <- if (is.null(filetype)) "json" else filetype
-
+parse_api <- function(response, filetype = c("csv", "json", "html", "txt"), view_html = FALSE) {
+  filetype <- match.arg(filetype)
 
   parsed <- switch(filetype,
                    "csv" = {
@@ -68,7 +72,27 @@ parse_api <- function(response, filetype = NULL) {
                    },
                    "json" = {
                      jsonlite::fromJSON(httr::content(response, "text", encoding = "UTF-8"))
-                   })
+                   },
+                   "html" = {
+                     if (view_html) {
+                       # Save content to a temporary HTML file
+                       html_file <- tempfile(fileext = ".html")
+                       writeBin(response$content, html_file)
+                       # Open the HTML file in the default system browser
+                       browseURL(html_file)
+                       return(invisible(html_file))  # Return file path invisibly if needed
+                     } else {
+                       # Just return raw HTML text
+                       rawToChar(response$content)
+                     }
+                   },
+                   "txt" = {
+                     raw_text <- rawToChar(response$content)
+                     utils::read.delim(textConnection(raw_text), sep = "\t")
+                   },
+                   stop("Unsupported filetype")
+  )
 
   return(parsed)
 }
+
