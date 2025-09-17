76c38101913e7383f03f79ddc9ea3780c36ac37e
Sarah Jessop <jessop.sarah.a@gmail.com>
2025-09-17 16:12:36 +1000

    dummy_media data documented

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' Dummy Media Data
+#'
+#' A subset of data from SQUIDLE that contains "point.media.key" that relate to a set of images.
+#' Used in POST workflow example in README.
+#'
+#' @format ## `dummy_media`
+#' A data frame with 131 obs. and 1 variable:
+#' \describe{
+#'   \item{point.media.key}{point media key}
+#' }
+"dummy_media"
